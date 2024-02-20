151ff808ae5a9978e753dfcae9c3a1dc746f2a25
Dmokrite1 <hg_greg@proton.me>
2024-02-20 13:00:31 +0100

    exercice progress running

--- control-flow/src/app/app.component.ts
+++ control-flow/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { BooksComponent } from './books/books.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, BooksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -22,6 +23,11 @@ export class AppComponent {
     title: "★☆★ Amaury goes to Holywood ★☆★",
     releaseYear: 1997,
     genre: "★☆ porno ☆★",
+  }, {
+    title: "la louloutre contre-attaque",
+    releaseYear: 2158,
+    genre: "bien chelou"
   }];
   evilOtters = [];
+  viewMode: 'kanban' | 'list' | 'default' = 'default'
 }
